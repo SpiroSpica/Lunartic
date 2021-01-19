ff2330597ffa2948d5b9b3e46387e70ac137c506
Twinstar <hath1997@gmail.com>
2021-01-19 19:38:15 +0900

    EnemySpawner
    EnemySpawner Implemented

---
+++ Source/Lunartic/MonsterSpawner.cpp
@@ -0,0 +1,72 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MonsterSpawner.h"
+
+// Sets default values
+AMonsterSpawner::AMonsterSpawner()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryActorTick.bCanEverTick = true;
+
+	BuildingMesh = CreateDefaultSubobject<UStaticMeshComponent>("BuildingMesh");
+	SpawnPoint = CreateDefaultSubobject<UParticleSystemComponent>("SpawnPoint");
+
+	SpawnInterval = 0.01;
+
+	auto MeshAsset = ConstructorHelpers::FObjectFinder<UStaticMesh>(TEXT("StaticMesh'/Engine/BasicShapes/Cube.Cube'"));
+	if (MeshAsset.Object != nullptr)
+	{
+		BuildingMesh->SetStaticMesh(MeshAsset.Object);
+		//BuildingMesh->SetCollisionProfileName(UCollisionProfile::Pawn_ProfileName);
+	}
+
+	auto ParticleSystem = ConstructorHelpers::FObjectFinder<UParticleSystem>(TEXT("ParticleSystem'/Engine/Tutorial/SubEditors/TutorialAssets/TutorialParticleSystem.TutorialParticleSystem'"));
+	if (ParticleSystem.Object != nullptr)
+	{
+		SpawnPoint->SetTemplate(ParticleSystem.Object);
+	}
+
+	SpawnPoint->SetRelativeScale3D(FVector(0.5, 0.5, 0.5));
+
+	static ConstructorHelpers::FObjectFinder<UBlueprint> Unit(TEXT("'/Game/Trial.Trial'"));
+	if (Unit.Object != nullptr)
+	{
+		UnitToSpawn = (UClass*)Unit.Object->GeneratedClass;
+	}
+}
+
+// Called when the game starts or when spawned
+void AMonsterSpawner::BeginPlay()
+{
+	Super::BeginPlay();
+
+	RootComponent = BuildingMesh;
+	SpawnPoint->AttachToComponent(RootComponent, FAttachmentTransformRules::KeepRelativeTransform);
+	SpawnPoint->SetRelativeLocation(FVector(150, 0, 0));
+	GetWorld()->GetTimerManager().SetTimer(SpawnTimerHandle, this, &AMonsterSpawner::SpawnUnit, SpawnInterval, true);
+
+	
+}
+
+void AMonsterSpawner::SpawnUnit()
+{
+	FVector SpawnLocation = SpawnPoint->GetComponentLocation();
+	GetWorld()->SpawnActor(UnitToSpawn, &SpawnLocation);
+}
+
+
+void AMonsterSpawner::EndPlay(const EEndPlayReason::Type EndPlayReason)
+{
+	Super::EndPlay(EndPlayReason);
+	GetWorld()->GetTimerManager().ClearTimer(SpawnTimerHandle);
+}
+
+// Called every frame
+void AMonsterSpawner::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
